cb1bba8913b3fac07724e2525b0974df0642c07b
nemyr <misha.zhban@gmail.com>
2023-07-24 19:39:04 +0300

    Добавьте файлы проекта.

---
+++ Pixelizer/Data/ProcessingSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Pixelizer.Data
+{
+    public class ProcessingSettings
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public bool KeepAspectRatio { get; set; }
+        public int Colors { get; set; }
+    }
+}
