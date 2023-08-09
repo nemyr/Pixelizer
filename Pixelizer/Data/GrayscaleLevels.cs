ac18aa2a613614e0e33d36b89bad71df7197259e
nemyr <misha.zhban@gmail.com>
2023-08-10 01:28:13 +0300

    Changed default settings

--- Pixelizer/Data/GrayscaleLevels.cs
+++ Pixelizer/Data/GrayscaleLevels.cs
@@ -2,8 +2,8 @@
 {
     public class GrayscaleLevels
     {
-        public float Red { get; set; } = 1.0f;
-        public float Green { get; set; } = 1.0f;
-        public float Blue { get; set; } = 1.0f;
+        public float Red { get; set; } = .2126f;
+        public float Green { get; set; } = .7152f;
+        public float Blue { get; set; } = .0722f;
     }
 }
--- Pixelizer/Data/ProcessingSettings.cs
+++ Pixelizer/Data/ProcessingSettings.cs
@@ -7,6 +7,8 @@
         public int Height { get; set; } = 10;
         public bool KeepAspectRatio { get; set; }
         public int Colors { get; set; } = 5;
+        public bool UseGrayscale { get; set; } = false;
+        public bool UsePalette { get; set; } = true;
         public GrayscaleLevels GrayscaleLevels { get; set; } = new GrayscaleLevels();
     }
 }
